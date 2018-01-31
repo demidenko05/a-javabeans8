49172de6cf1fcfb9b1ab56eb5738c0da29b86045
Yury Demidenko <demidenko05@gmail.com>
2018-01-31 10:33:44 +0300

    deploy.ssh fixed

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -9,9 +9,9 @@ if ( [ "$REPLY" == "yes" ] ) then
   ssh-add ~/.ssh/$sshkeyfile
   ssh-add -l
   read -p "Enter GPG keyname: " gpgkeynm
-  read -p "Enter key alias sign JAR/APK: " keyalias
-  read -s -p "Enter pass-phrase to sign JAR/APK: " passw
-  mvn -Darguments="-Prelease -Dandroid.release=true -Dsignpass=$passw -Dsignalias=$keyalias -Dgpgkeyname=$gpgkeynm" release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+  read -p "Enter key alias sign JAR/APK: " jkeyalias
+  read -p "Enter pass-phrase to sign JAR/APK: " jpassw
+  mvn -Darguments="-Prelease -Dandroid.release=true -Dgpgkeyname=$gpgkeynm -Dsignalias=$jkeyalias -Dsignpass=$jpassw" release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
 else
   echo 'Exit without deploy'
