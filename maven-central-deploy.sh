4fb3598cfbf48d7777c46e37808ef18c9a4ada4a
Yury Demidenko <demidenko05@gmail.com>
2018-01-31 11:02:27 +0300

    pom release conf fixed

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -11,7 +11,7 @@ if ( [ "$REPLY" == "yes" ] ) then
   read -p "Enter GPG keyname: " gpgkeynm
   read -p "Enter key alias sign JAR/APK: " jkeyalias
   read -p "Enter pass-phrase to sign JAR/APK: " jpassw
-  mvn -Darguments="-Prelease -Dandroid.release=true -Dgpgkeyname=$gpgkeynm -Dsignalias=$jkeyalias -Dsignpass='$jpassw'" release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+  mvn -Darguments="-Prelease -Dandroid.release=true -Dgpgkeyname=$gpgkeynm -Dsignalias=$jkeyalias -Dsignpass=$jpassw" release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
 else
   echo 'Exit without deploy'
